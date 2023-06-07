ed868a5e715ac2f401288f72e64eb8c17c770dc1
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-07 20:18:54 +0100

    used floyd's algo to solve this, had to resarch

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,10 +1,30 @@
-#include<lists.h>
+#include "lists.h"
+
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
+ * check_cycle - checks if a singly linked list has a cycle in it using floyd
+ * algorithm
  * @list: linked list
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	
+	listint_t *slownode, *fastnode;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	slownode = list;
+	fastnode = list->next;
+
+	while (fastnode != NULL && fastnode->next != NULL)
+	{
+		if (slownode == fastnode)
+			return (1);
+
+		slownode = slownode->next;
+		fastnode = fastnode->next->next;
+	}
+
+	return (0);
 }
+
