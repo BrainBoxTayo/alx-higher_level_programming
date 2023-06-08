c3680c70a2998c82b7734afb35af6f807e459f32
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-08 19:19:41 +0100

    understood scripts are run as __main__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import sys
 import add_0
-if __name__ != "__main__":
-    sys.exit(0)
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
