42c8f8f3bf321115b0ab932b03ba092126ae266c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-08 20:34:00 +0100

    iamdone

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-import hidden_4
-str = dir(hidden_4)
-for i in range(1, len(str)):
-    print("{}", str[i])
+if __name__ == "__main__":
+    import hidden_4
+    str = dir(hidden_4)
+    for i in str:
+        if i[:2] != "__":
+            print(i)
