04efe780726f56c46202e85b43bc43d6d6f72221
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-10 18:27:46 +0100

    modifies a list without affecting the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copycat = my_list.copy()
     if idx < 0:
         return copycat
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return copycat
     else:
         copycat[idx] = element
