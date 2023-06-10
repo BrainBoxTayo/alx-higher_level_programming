04c29c4a004bdd6878c9e5a578230fb285b5cd0f
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-10 17:18:17 +0100

    fixed edge cases

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,11 @@ def element_at(my_list, idx):
     '''retrieves an element from a list'''
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
+    elif idx == len(my_list) - 1:
+        a = my_list[idx]
+        return a[0]
     else:
         a = my_list[idx:idx+1]
         return a[0]
