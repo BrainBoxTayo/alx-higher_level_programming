d125f85b754d417a5651328d0ba4eed7b1f05f32
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-11 19:34:52 +0100

    chmod 777 errror

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j == matrix[-1]:
-                print("{:1d}".format(j), end="")
+                print("{:d}".format(j), end="")
                 continue
-            print("{:1d} ".format(j), end="")
+            print("{:d} ".format(j), end="")
         print()
