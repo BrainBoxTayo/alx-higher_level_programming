da6b41b574f65b9a85fe29ae1fe2983067d84375
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-26 20:08:12 +0100

    Zero Division Error Handling and finally keyword

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         c = a / b
     except (ZeroDivisionError, TypeError):
-        return None
+        c = None
     finally:
         print("Inside result: {}".format(c))
     return c
