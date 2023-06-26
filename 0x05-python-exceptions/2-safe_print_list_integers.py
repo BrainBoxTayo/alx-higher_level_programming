a74f43d4c0d83f87a4d6802cc8c12bfdfe378b3f
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-26 19:53:49 +0100

    value type and Index errors all handled with index raised

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         except (TypeError, ValueError):
             x = x - 1
-        except(IndexError):
+        except (IndexError):
             x = x - 1
             raise
     print("\n", end="")
