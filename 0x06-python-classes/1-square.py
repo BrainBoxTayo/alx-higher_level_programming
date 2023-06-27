12a2b2c033189a9601e0b47ebc486fd4ed847bd1
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-27 23:00:39 +0100

    empty square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class of type square with size attribute"""
+
+
+class Square:
+
+    """Representing a square"""
+    def __init__(self, size):
+
+        """initializer for class square"""
+        self.__size = size
