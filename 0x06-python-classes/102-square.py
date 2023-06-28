8e45dc45c660e26c62becda29b7e52c656ac0985
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-28 17:10:07 +0100

    other magic functions for comparison

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Class of type square with size attribute"""
+
+
+class Square:
+
+    """Representing a square"""
+
+    def __init__(self, size=0):
+        """initializer for class square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for __size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for __size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Area for the square"""
+        return (self.__size * self.__size)
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
