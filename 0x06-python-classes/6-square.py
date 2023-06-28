2e3fcc3fe51df72b6ae386f16aa2e1eac4e98446
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-28 16:25:09 +0100

    linked list with sorted insert

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,23 +8,12 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """initializer for class square"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        elif (not isinstance(position, tuple) or
-            len(position) is not 2 or
-            not all(num >= 0 for num in position) or
-                not all(isinstance(num, int) for num in position)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__size = size
-            self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         """Getter for __size"""
-
         return self.__size
 
     @size.setter
