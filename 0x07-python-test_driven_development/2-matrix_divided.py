2847f631fa3cbe844828518763cb907cc5848cc1
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-02 22:29:43 +0100

    matrix problem solved and tests carried out

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,10 +4,44 @@ divide all elements of a matrix module
 '''
 
 
-def matrix_divided(matrix,div):
+def matrix_divided(matrix, div):
     '''
-    function to do the task given above
+     Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
     '''
-    matrix = [["billy"], [90,23]]
-    if not all(isinstance([[number for number in group] for group in matrix], (int, float))):
-        print("Yo")
+    typeError1 = "matrix must be a matrix (list of lists) of integers/floats"
+    new_mat = []
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    length = len(matrix)
+    if (length < 2):
+        # handles if the list of list passed is only a single list
+        #  in another list
+        raise TypeError(typeError1)
+    for i in range(0, length):
+        # this checks if all the lists are equal in size
+        if not isinstance(matrix[i], list):
+            raise TypeError(typeError1)
+        if i + 1 == length:
+            break
+        if len(matrix[i]) != len(matrix[i + 1]):
+            raise TypeError("Each row of the matrix must have the same size")
+    for i in range(0, len(matrix)):
+        # this is where the division happens
+        inner_list = []
+        for j in range(0, len(matrix[i])):
+            if not isinstance(matrix[i][j], (int, float)):
+                raise TypeError(typeError1)
+            inner_list.append(round((matrix[i][j] / div), 2))
+        new_mat.append(inner_list)
+    return new_mat
