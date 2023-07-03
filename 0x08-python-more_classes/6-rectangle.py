443f3408935143699987e8f6061f10e8a56a3bac
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-03 19:38:03 +0100

    added a number of instances counter

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+Defines a Rectangle
+"""
+
+
+class Rectangle:
+    """
+    this is the class Rectangle
+    attributes:
+        __width: instance attribute
+        __height: instance attribute
+        number_of_instances: the number of instances of class Rectangle
+    methods:
+        area: returns area of the rectangle
+        perimeter: returns the perimeter of the rectangle
+    """
+    number_of_instances = 0
+
+    # the Underscores
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return '\n'.join(['#' * self.width for i in range(self.height)])
+
+    def __repr__(self):
+        return ('Rectangle(' + str(self.width) + ', ' + str(self.height) + ')')
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
+
+    # decorated methods
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    # methods to calculate stuff
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.height + self.width)
