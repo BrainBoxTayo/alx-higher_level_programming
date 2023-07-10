a62ced06160c4856909c76fffead4de201433c5e
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:04:10 +0100

    prints a sorted list

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+inheritance learning module
+"""
+
+
+def is_same_class(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is a_class:
+        return True
+    else:
+        return False
