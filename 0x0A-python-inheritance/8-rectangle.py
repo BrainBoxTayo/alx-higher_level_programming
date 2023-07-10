a1d5157ee9a7de69a907ac160c9c4cdfb814e566
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:49:56 +0100

    inheritance adn the super()

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(BaseGeometry):
     """class for rectangles"""
     def __init__(self, width, height):
         """initializes a new rectangle"""
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
