8469c057b21e40ff1249716cba8edb118cc1033a
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 21:07:03 +0100

    the super() duper

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -43,6 +43,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initializer for the square"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
