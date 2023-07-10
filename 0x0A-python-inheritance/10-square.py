a579e5309d04522ba133be22031db29a658205e6
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 21:09:22 +0100

    the super() duper

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,42 +1,6 @@
 #!/usr/bin/python3
-"""
-Class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """The base geometry class"""
-
-    def area(self):
-        """area function"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer validator funcrion"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class for rectangles"""
-
-    def __init__(self, width, height):
-        """initializes a new rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """area implementation"""
-        return self.__width * self.__height
-
-    def __repr__(self):
-        """repr to return a string on print"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
+"""The square subclass of the Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """The class for a square"""
