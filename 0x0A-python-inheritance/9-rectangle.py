dd809913bb3706b03e0073b111aaac4bacab1b19
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:56:34 +0100

    some rectangle methds

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,17 +21,18 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """class for rectangles"""
+
     def __init__(self, width, height):
         """initializes a new rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
         """area implementation"""
         return self.__width * self.__height
-    
+
     def __repr__(self):
         """repr to return a string on print"""
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
