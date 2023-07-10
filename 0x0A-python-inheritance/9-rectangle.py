7a9ecb6cad4a58f4edc6ffbd77794b6bfdc9c4c2
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:54:47 +0100

    some rectangle methds

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """The base geometry class"""
+
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator funcrion"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class for rectangles"""
+    def __init__(self, width, height):
+        """initializes a new rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __repr__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
