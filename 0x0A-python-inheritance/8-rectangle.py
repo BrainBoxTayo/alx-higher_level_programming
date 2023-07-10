0e095837de323479aae2313bf23818fbe72bd1c4
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:46:37 +0100

    inheritance adn the super()

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """The base geometry class"""
+
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator funcrion"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class for rectangles"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__height = height
+        self.__width = width
