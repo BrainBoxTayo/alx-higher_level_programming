05c4cdf46eb3d8f6dbf23d4cf5253c9ed7e30875
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:19:49 +0100

    BasGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """The base geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
