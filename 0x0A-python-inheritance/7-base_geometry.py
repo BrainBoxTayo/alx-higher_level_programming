334b4526cb50dfd88a6190a016929fcda58f80d9
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:38:24 +0100

    area exception sc

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,10 +6,14 @@ Class BaseGeometry
 
 class BaseGeometry:
     """The base geometry class"""
+
     def area(self):
+        """area function"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        if not isinstance(value, int) and type(value) != int:
+        """integer validator funcrion"""
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
