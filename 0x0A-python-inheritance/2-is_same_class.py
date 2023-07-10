7f8b418d4b11f4361d061daf209a8d6b8985f3db
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 20:04:51 +0100

    prints a sorted list

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,7 @@ inheritance learning module
 
 
 def is_same_class(obj, a_class):
+    """is it the same class"""
     if isinstance(obj, a_class) and type(obj) is a_class:
         return True
     else:
