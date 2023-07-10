74254a08530dd70fb80612622dd276884d80c938
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-10 21:11:27 +0100

    the super() duper

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-"""The rectangle subclass of the BaseGeometry class"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """The base geometry class"""
+
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator funcrion"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
