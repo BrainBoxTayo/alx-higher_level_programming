040af26176a4acbd2a7eae1bae3f8e6427fd8712
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-11 20:56:02 +0100

    To json with some filtering

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+module to define a class student
+'''
+
+
+class Student:
+    ''' The class Student '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initialization Module'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''same as 8-class_to_json'''
+        dicts = {}
+        if attrs:
+            for j in attrs:
+                if hasattr(self, j):
+                    dicts[j] = getattr(self, j)
+                else:
+                    continue
+            return (dicts)
+        else:
+            return (vars(self))
