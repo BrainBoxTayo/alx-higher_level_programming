36f5016ef96f809991d39893c9358ba0d3731e32
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-11 19:08:30 +0100

    this writes to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ module to write strings
 
 
 def write_file(filename="", text=""):
+    """function that writes text to filename"""
     with open(filename, 'w', encoding="utf-8") as f:
         no_char = f.write(text)
     return no_char
