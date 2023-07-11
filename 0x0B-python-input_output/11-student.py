fd69bc06e00996571be24bc56a9cbef2cf6ce228
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-11 21:14:44 +0100

    Serialize Deserialize

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+module to define a class student
+'''
+
+
+class Student:
+    ''' The class Student '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initialization Module'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''same as 8-class_to_json'''
+        dicts = {}
+        if type(attrs) == list:
+            for j in attrs:
+                if hasattr(self, j):
+                    dicts[j] = getattr(self, j)
+                else:
+                    continue
+            return (dicts)
+        else:
+            return (vars(self))
+    
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
