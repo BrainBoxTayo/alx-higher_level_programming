1a62c52b483b97e92cfad874eecdd903605148c7
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-11 19:01:32 +0100

    something that we dreamt of

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ module for reading and printing a file
 def read_file(filename=""):
     '''function that does the task'''
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
