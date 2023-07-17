77e092d26ebbda7f1ae16bb59d3e70424343319a
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-17 20:13:07 +0100

    new tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,122 +1,141 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
-"""
-import os
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
 from models.square import Square
+import json
+import inspect
+
+'''
+    Creating test cases for the base module
+'''
 
 
-class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+class test_base(unittest.TestCase):
+    '''
+        Testing base
+    '''
 
-    def test_no_arg(self):
-        """Test instantation with no argument."""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_id_none(self):
+        '''
+            Sending no id
+        '''
+        b = Base()
+        self.assertEqual(1, b.id)
 
-    def test_three_bases(self):
-        """Test instantiation of 3 bases."""
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 2)
+    def test_id(self):
+        '''
+            Sending a valid id
+        '''
+        b = Base(50)
+        self.assertEqual(50, b.id)
 
-    def test_None_id(self):
-        """Test instantiation with id as None."""
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_id_zero(self):
+        '''
+            Sending an id 0
+        '''
+        b = Base(0)
+        self.assertEqual(0, b.id)
 
-    def test_unique_id(self):
-        """Test unique ids."""
-        self.assertEqual(12, Base(12).id)
+    def test_id_negative(self):
+        '''
+            Sending a negative id
+        '''
+        b = Base(-20)
+        self.assertEqual(-20, b.id)
 
-    def test_nb_instances_after_unique_id(self):
-        """Test nb_instances after setting a unique id."""
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
+    def test_id_string(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base("Betty")
+        self.assertEqual("Betty", b.id)
 
-    def test_id_public(self):
-        """Test id as public instance attribute."""
-        b = Base(12)
-        b.id = 15
-        self.assertEqual(15, b.id)
+    def test_id_list(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
 
-    def test_nb_instances_private(self):
-        """Test nb_instances as private instance attribute."""
-        with self.assertRaises(AttributeError):
-            print(Base(12).__nb_instances)
+    def test_id_dict(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base({"id": 109})
+        self.assertEqual({"id": 109}, b.id)
 
-    def test_str_id(self):
-        """Test instantiation with str id."""
-        self.assertEqual("hello", Base("hello").id)
+    def test_id_tuple(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
 
-    def test_float_id(self):
-        """Test instantiation with float id."""
-        self.assertEqual(5.5, Base(5.5).id)
+    def test_to_json_type(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
 
-    def test_complex_id(self):
-        """Test instantiation with complex id."""
-        self.assertEqual(complex(5), Base(complex(5)).id)
+    def test_to_json_value(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
 
-    def test_dict_id(self):
-        """Test instantiation with dict id."""
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+    def test_to_json_None(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
 
-    def test_bool_id(self):
-        """Test instantiation with bool id."""
-        self.assertEqual(True, Base(True).id)
-
-    def test_list_id(self):
-        """Test instantiation with list id."""
-        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
-
-    def test_tuple_id(self):
-        """Test instantiation with tuple id."""
-        self.assertEqual((1, 2), Base((1, 2)).id)
-
-    def test_set_id(self):
-        """Test instantiation with set id."""
-        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
-
-    def test_frozenset_id(self):
-        """Test instantiation with frozenset id."""
-        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
-
-    def test_range_id(self):
-        """Test instantiation with range id."""
-        self.assertEqual(range(5), Base(range(5)).id)
-
-    def test_bytes_id(self):
-        """Test instantiation with bytes id."""
-        self.assertEqual(b'Python', Base(b'Python').id)
-
-    def test_bytearray_id(self):
-        """Test instantiation with bytearray id."""
-        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
-
-    def test_memoryview_id(self):
-        """ Test instantiation with memoryview id."""
-        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
-
-    def test_inf_id(self):
-        """ Test instantiation with float('inf') id."""
-        self.assertEqual(float('inf'), Base(float('inf')).id)
-
-    def test_NaN_id(self):
-        """ Test instantiation with float('nan') id."""
-        self.assertNotEqual(float('nan'), Base(float('nan')).id)
-
-    def test_two_args(self):
-        """Test instantiation with two arguments."""
-        with self.assertRaises(TypeError):
-            Base(1, 2)
+    def test_to_json_Empty(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class' methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """
+        Tests if methods docstring documntation exist
+        """
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)
