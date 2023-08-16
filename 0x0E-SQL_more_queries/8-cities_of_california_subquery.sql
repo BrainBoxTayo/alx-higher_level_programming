91356a3ad5777cc372f8252d070ba10e648eb625
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-08-16 17:52:51 +0100

    newer

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
--- inner join 
-SELECT cities.id, cities.name 
-FROM states 
-INNER JOIN cities ON cities.state_id = states.id
+-- inner join
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
