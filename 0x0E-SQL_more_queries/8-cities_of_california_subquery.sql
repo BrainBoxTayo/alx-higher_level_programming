1052bf52aff450f786646f940f5a2b709a44664c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-08-16 17:51:04 +0100

    Inner Join Example

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
- SELECT cities.id, cities.name 
- FROM states 
- INNER JOIN cities ON cities.state_id = states.id
- ORDER BY cities.id ASC;
+-- inner join 
+SELECT cities.id, cities.name 
+FROM states 
+INNER JOIN cities ON cities.state_id = states.id
+ORDER BY cities.id ASC;
