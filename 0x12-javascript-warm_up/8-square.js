4799595d9bfe749441b87ddec2befc9a0acf7621
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-11 19:50:42 +0100

    corrected the number issue

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let num = '';
+if (process.argv.length > 2 && (typeof process.argv[2][0] !== 'undefined')) {
+  num = process.argv[2][0].charCodeAt(0);
+}
+if (num >= 48 && num <= 57) {
+  num = parseInt(process.argv[2]);
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+} else if (num === 45) {
+  process.exit(0);
+} else {
+  console.log('Missing size');
+}
