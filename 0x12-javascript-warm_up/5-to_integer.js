4858ba5ef16bcfdf994e5e74d4e8827f07eeb69e
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-11 18:33:33 +0100

    corrected the number issue

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ if (process.argv.length > 2) {
   num = process.argv[2][0].charCodeAt(0);
 }
 if (num >= 48 && num <= 57) {
-  console.log(parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
