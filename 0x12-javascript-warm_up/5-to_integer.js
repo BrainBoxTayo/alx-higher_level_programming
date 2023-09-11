9c988f984fd06f5b1ab647de70b011e813f10571
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-11 19:35:10 +0100

    fixed maybe?

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,6 +5,8 @@ if (process.argv.length > 2 && (typeof process.argv[2][0] !== 'undefined')) {
 }
 if (num >= 48 && num <= 57) {
   console.log('My number: ' + parseInt(process.argv[2]));
+} else if (num === 45) {
+  process.exit(0);
 } else {
   console.log('Not a number');
 }
