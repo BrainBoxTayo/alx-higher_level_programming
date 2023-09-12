b36dde5c847acaed553b38b1fe6f93bc9af3ef04
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-12 18:33:30 +0100

    fixed isNan

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@ class Rectangle {
   width;
   height;
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       //  pass
     } else if (w === undefined || h === undefined) {
       //  pass
