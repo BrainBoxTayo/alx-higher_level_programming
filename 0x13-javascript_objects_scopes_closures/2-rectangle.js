be35637dd2fe5fb5ec7c8265e7be862b53e95823
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-12 18:43:10 +0100

    Please enter a commit message

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,7 @@ class Rectangle {
   width;
   height;
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      //  pass
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
