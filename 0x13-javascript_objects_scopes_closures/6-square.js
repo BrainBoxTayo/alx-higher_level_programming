aa43436a9d2f8285663ef52f4f79a23bdbbe23f5
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-12 20:24:21 +0100

    semistandard🤷‍

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const square = require('./5-square');
 
 class Square extends square {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
