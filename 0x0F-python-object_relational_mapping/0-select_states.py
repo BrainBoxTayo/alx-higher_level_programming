9a8c343bbc78ef73203c9e838dbc7d0f2fff4ecd
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-14 17:43:49 +0100

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
@@ -18,6 +19,6 @@ if __name__ == "__main__":
 
     for row in results:
         print(row)
-    
+
     cursor.close()
     db.close()
