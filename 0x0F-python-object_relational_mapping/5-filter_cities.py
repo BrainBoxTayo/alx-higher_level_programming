7ba9411ebc4d4fbc34a0661e485281e881ad6a0d
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-09-14 18:53:30 +0100

    5-filter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+'''
+    script that lists all states lists all states with a name starting with N
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sta_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    query = "SELECT  cities.name FROM cities LEFT JOIN states\
+                ON cities.state_id = states.id WHERE states.name = %s\
+                ORDER BY cities.id"
+    cursor.execute(query, (sta_name, ))
+    results = cursor.fetchall()
+    tmp = []
+    for row in results:
+        tmp.append(row[0])
+    print(*tmp, sep=", ")
+    cursor.close()
+    db.close()
