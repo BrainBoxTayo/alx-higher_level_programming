3e86860e8e19dd174883451b0948fe27f7575101
Akanbi-Bello Temitayo <122236794+BrainBoxTayo@users.noreply.github.com>
2023-11-07 18:38:12 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-// prints a string myVar
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function factorial (number) {
-  if (isNaN(number) || number === 0) {
-    return (1);
-  } else {
-    return (number * factorial(number - 1));
-  }
-}
-console.log(factorial(Number(process.argv[2])));
